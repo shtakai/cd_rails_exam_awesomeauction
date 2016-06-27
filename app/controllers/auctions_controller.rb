0b352a196d583592b5353441b10f4cb983f3b1e5
Alyson Takai <shtakai@gmail.com>
2016-06-27 16:45:18 -0700

    after creattin bid

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -3,7 +3,22 @@ class AuctionsController < ApplicationController
 
   def index
     @auctions = Auction.running.page(params[:page])
+  end
 
+  def destroy
+    auction = Auction.find_by id: params[:id]
+    if auction.destroy_with_user session[:user_id]
+      logger.debug "auction #{auction.id} deleted"
+      flash[:notice] = "auction #{auction.id} #{auction.product_name} deleted"
+    else
+      logger.debug "auction deletion failed #{auction.id}"
+      flash[:alert] = "auction deletion failed #{auction.id}"
+    end
+    redirect_to '/auctions'
+  end
+
+  def show
+    @auction = Auction.find_by id: params[:id]
   end
 
 end
