057de7e51ed36f57e4f67c8d3c4f44d096423cd1
Alyson Takai <shtakai@gmail.com>
2016-06-27 11:03:12 -0700

    modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# clean
+User.destroy_all
+Auction.destroy_all
+Wallet.destroy_all
+Bid.destroy_all
+
+progressbar = ProgressBar.create
+
+
+# 1st user
+test_user = User.create(
+  username: 'test',
+  first_name: 'testf',
+  last_name: 'testl',
+  email: 'a@a.com',
+  password: 'testtest',
+  password_confirmation: 'testtest'
+)
+progressbar.increment
+
+# 1st user has some auctions
+5.times do
+  a = Auction.create(
+    product_name: Faker::Commerce.product_name[0..18],
+    description: Faker::Hipster.sentence,
+    starting_bid: 10,
+    reserve_price: rand(100..5000),
+    end_date: Faker::Date.forward(100),
+    user: test_user
+  )
+  progressbar.increment
+end
