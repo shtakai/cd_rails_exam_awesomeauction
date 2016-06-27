6962ded5c2ebd3ff354e8cb32539b82f1344051e
Alyson Takai <shtakai@gmail.com>
2016-06-27 16:58:58 -0700

    cannot bid that highest price is own

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,9 +2,11 @@ class Bid < ActiveRecord::Base
   belongs_to :user
   belongs_to :auction
 
+
   validate :highest_price
   validates :price, presence: true, uniqueness: true, numericality: {greater_than: 0}
   validate :avoid_own_bid
+  validate :avoid_highest_own_bid
 
   private
 
@@ -20,6 +22,17 @@ class Bid < ActiveRecord::Base
     return if price.blank?
     if auction.present? && auction.highest_bid.price > price
       errors.add(:bids, "cannot bid lower price of the highest_bid")
+    end
+  end
+
+  def avoid_highest_own_bid
+    # checks last (highest ) bid was made by own?
+    if auction.present? && auction.highest_bid[:user].present?
+      logger.debug "BID USER: #{auction.highest_bid[:user].id}"
+      logger.debug " user_id for creation; #{user_id}"
+      if auction.highest_bid[:user].id == user_id
+        errors.add(:bids, "cannot bid over own highest bid")
+      end
     end
   end
 
