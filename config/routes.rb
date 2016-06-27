1048e5c623bbbfbb68e56323c574917ad70e1817
Alyson Takai <shtakai@gmail.com>
2016-06-27 16:43:18 -0700

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:create]
-  resources :auctions, only: [:index, :destroy]
-  resources :bids, only: [:destroy]
-  post 'bids/:id' =>  'bids#new', as: :new_bid
+  resources :auctions  do
+    resources :bids
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
