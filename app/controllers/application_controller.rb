68425ea6e25c251a73a8a63ea31ca5e08e262719
Alyson Takai <shtakai@gmail.com>
2016-06-28 09:02:18 -0700

    shared method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :balance, if: :user_signed_in?
+
   protected
 
   def authenticate_user!
@@ -21,6 +23,10 @@ class ApplicationController < ActionController::Base
 
   def current_user_id
     session[:user_id]
+  end
+
+  def balance
+    @balance = current_user.wallet.balance
   end
 
 
